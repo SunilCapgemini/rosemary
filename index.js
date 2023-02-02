88d0b9ce04a2c5c9dabbc50fcf5aef59c03d0f9a
SunilCapgemini <116062814+SunilCapgemini@users.noreply.github.com>
2023-02-02 16:04:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,5 +17,23 @@ export {
             }
         }
         return arr.slice(start, end + 1);
+    },
+    quickSort:(array){
+        if(array.length < = 1){
+            return array;
+        }
+        const pivot = array[array.length - 1];
+        const left = [];
+        const right = [];
+        
+        
+        for (let i 0;i < array.length - 1; i++){
+            if(array[i] < pivot){
+                left.push(array[i];
+            } else {
+                right.push(array[i];
+            }
+        }
+        return this.quickSort(left).concat(pivot, this.quickSort(right));
     }
 }
